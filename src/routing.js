48655cdaffa099a42caad74234dcbad296ec2374
kasirajan <thiru.kasi.live.in@gmail.com>
2026-02-24 21:32:54 +0530

    CallBack hook

--- src/routing.js
+++ src/routing.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Dashbord from "./compnets/pages/dashbord";
 import Product from "./compnets/pages/product";
 import Home from "./compnets/pages/home";
+import CallBack from "./compnets/hooks/callBack";
 
 const Routing = () => {
     return (
@@ -10,6 +11,8 @@ const Routing = () => {
                 <Route path="/" element={<Dashbord />} />
                 <Route path="/product" element={<Product />} />
                 <Route path="/home" element={<Home />} />
+                <Route path="/callback" element={<CallBack />} />
+
             </Routes>
         </div>
     )
