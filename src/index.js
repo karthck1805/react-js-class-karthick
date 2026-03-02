8e6c707857ab1a7e7aaa63d5813fab3b52dec75e
kasirajan <thiru.kasi.live.in@gmail.com>
2026-03-02 20:59:04 +0530

    add use Contex

--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import Routing from './routing';
+import UseContexdata from './compnets/hooks/useContex/useContex';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    {/* <App /> */}
+   <UseContexdata.Provider value={"This is the value from the useContexdata"}>
     <Routing />
+   </UseContexdata.Provider>
+
   </BrowserRouter>
-  // tree app - node 
 );
 
 
